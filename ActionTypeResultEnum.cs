822a1500f9ca9a5af4f93d37580a5114c3f8ba1b
pjzbrown <paul@exe-it.net>
2015-12-21 13:24:38 +0000

    An initial test comment

--- ActionTypeResultEnum.cs
+++ ActionTypeResultEnum.cs
@@ -1,5 +1,7 @@
 ﻿namespace Torchlight.Mvc5.Common.Libs
 {
+    // A set of enums used to determine which ListViewModel interface to invoke
+
     public enum ActionTypeResultEnum
     {
         Create,
